3d734084e0e009f6f0a931754b91595b80b98351
SergheiKot <sergheikotwork@gmail.com>
2020-09-07 15:16:08 +0300

    Little refactor

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# usm-java-tasks
-#USM
+### Java tasks
--- src/md/usm/javatasks/Main.java
+++ src/md/usm/javatasks/Main.java
@@ -1,3 +1,5 @@
+package md.usm.javatasks;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
