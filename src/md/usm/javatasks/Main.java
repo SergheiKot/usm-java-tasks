42eb2dea1357afc73914901c46b6b3ffdcb303de
SergheiKot <sergheikotwork@gmail.com>
2020-09-08 16:56:39 +0300

    Added task-2.16

--- src/md/usm/javatasks/Main.java
+++ src/md/usm/javatasks/Main.java
@@ -1,7 +1,21 @@
 package md.usm.javatasks;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please, enter the first number: ");
+        int firstNumber = input.nextInt();
+
+        System.out.print("Please, enter the second number: ");
+        int secondNumber = input.nextInt();
+
+        if (firstNumber != secondNumber) {
+            System.out.println((firstNumber > secondNumber ? firstNumber : secondNumber) + " is larger");
+        } else {
+            System.out.println("These numbers are equal");
+        }
     }
 }
