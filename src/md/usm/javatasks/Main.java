06bb5553175a14d23c023eeeef755bf495243f50
SergheiKot <sergheikotwork@gmail.com>
2020-09-08 16:48:19 +0300

    Added task-2.15

--- src/md/usm/javatasks/Main.java
+++ src/md/usm/javatasks/Main.java
@@ -1,7 +1,20 @@
 package md.usm.javatasks;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please, enter the first number: ");
+        int firstNumber = input.nextInt();
+
+        System.out.print("Please, enter the second number: ");
+        int secondNumber = input.nextInt();
+
+        System.out.println("Sum: " + (firstNumber + secondNumber));
+        System.out.println("Product: " + (firstNumber * secondNumber));
+        System.out.println("Difference: " + (firstNumber - secondNumber));
+        System.out.println("Quotient: " + (firstNumber / secondNumber));
     }
 }
