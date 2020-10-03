c14f0011d1c3ed0f02a64aedc157f22628c56fb2
SergheiKot <sergheikotwork@gmail.com>
2020-10-03 21:42:54 +0300

    Added task-7.14

--- src/md/usm/javatasks/Main.java
+++ src/md/usm/javatasks/Main.java
@@ -2,6 +2,22 @@ package md.usm.javatasks;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.println("1 * 2 * 3 * 4 * 5 = " + product(1, 2, 3, 4, 5));
+
+        System.out.println("3 * 3 * 3 * 3 = " + product(3, 3, 3, 3));
+
+        System.out.println("1 * 0 * 1 = " + product(1, 0, 1));
+
+        System.out.println("-1 * 1 = " + product(-1, 1));
+    }
+
+    public static int product(int... args) {
+        int result = 1;
+
+        for (int value : args) {
+            result *= value;
+        }
+        return result;
     }
 }
