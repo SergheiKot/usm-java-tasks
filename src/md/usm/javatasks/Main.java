03be3cd2b1d921a7ad5f5fa6436edd04443b476a
SergheiKot <sergheikotwork@gmail.com>
2020-10-03 20:36:50 +0300

    Added task-5.11

--- src/md/usm/javatasks/Main.java
+++ src/md/usm/javatasks/Main.java
@@ -1,7 +1,25 @@
 package md.usm.javatasks;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of values to sort: ");
+        int numValues = scanner.nextInt();
+
+        System.out.print("Enter values: ");
+        int smallest = scanner.nextInt();
+
+        while (numValues > 1) {
+
+            System.out.print("Enter values: ");
+            smallest = Math.min(scanner.nextInt(), smallest);
+
+            numValues--;
+        }
+
+        System.out.println("The smallest value is " + smallest);
     }
 }
