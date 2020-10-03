cb8968fda3a0269ee05ea0195c8fa790951566b7
SergheiKot <sergheikotwork@gmail.com>
2020-10-03 20:50:08 +0300

    Added task-5.16

--- src/md/usm/javatasks/Main.java
+++ src/md/usm/javatasks/Main.java
@@ -1,7 +1,25 @@
 package md.usm.javatasks;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        int[] values = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            values[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.printf("%d: ", i + 1);
+
+            for (int j = 0; j < values[i]; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
