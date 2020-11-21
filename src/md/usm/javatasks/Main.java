95335ed980c0e2a841d59527864ad55645adf738
SergheiKot <sergheikotwork@gmail.com>
2020-11-21 19:25:05 +0200

    Added task-14.9

--- src/md/usm/javatasks/Main.java
+++ src/md/usm/javatasks/Main.java
@@ -1,7 +1,17 @@
 package md.usm.javatasks;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.print("Please enter a text: ");
+        Scanner scanner = new Scanner(System.in);
+
+        String[] inputWords = scanner.nextLine().split(" ");
+
+        for (int i = inputWords.length - 1; i >= 0; i--) {
+            System.out.print(inputWords[i] + " ");
+        }
     }
 }
