3b82e1a71724c258082bd18dbff8d9f42aaa0c3a
SergheiKot <sergheikotwork@gmail.com>
2020-11-21 19:15:21 +0200

    Added task-14.7

--- src/md/usm/javatasks/Main.java
+++ src/md/usm/javatasks/Main.java
@@ -1,7 +1,26 @@
 package md.usm.javatasks;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static void printLatinWord(String token) {
+
+        token = token.concat(String.valueOf(token.charAt(0)))
+                .replaceFirst(String.valueOf(token.charAt(0)), "");
+
+        System.out.print(token + "ay" + " ");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.print("Please enter a text: ");
+        Scanner scanner = new Scanner(System.in);
+
+        String[] inputWords = scanner.nextLine().split(" ");
+
+        for (String token : inputWords) {
+            printLatinWord(token);
+        }
     }
 }
